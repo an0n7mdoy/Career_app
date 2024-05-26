f0218fb7f9607552ff3dfe535f07e1876b41e557
an0n7mdoy <sadukovs@inbox.ru>
2024-05-26 18:55:40 +0300

    Initial Commit

---
+++ Career_app/Career_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Career_appApp.swift
+//  Career_app
+//
+//  Created by Simonchik on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Career_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
