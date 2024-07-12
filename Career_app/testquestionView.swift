285f9cc6e4d7a4fc870a3b725af2455f5b6d0510
anastasia <polevayapas@yandex.ru>
2024-07-12 03:02:22 +0300

    testquestionView + text + answers + images of buttons (no real buttons yet)

--- Career_app/testquestionView.swift
+++ Career_app/testquestionView.swift
@@ -21,46 +21,158 @@ struct testquestionView: View {
                             .frame(width: 450, height: 305)
                             .clipped()
                                 )
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
+                ZStack {
+                    Rectangle()
+                        .frame(width: 300.0, height: 70.0)
+                        .cornerRadius(15)
+                        .foregroundColor(.init(red: 0.236, green: 0.266, blue: 0.286))
+                    
+                    
+                    Text("Career Guidance Test")
+                        .font(Font.custom("Livvic-Regular", size: 29))
+                        .lineLimit(0)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color(red: 0.54, green: 0.57, blue: 0.58))
+                }
+
+    
+               
          
                 ZStack {
                     Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 364, height: 518.74799)
-                        .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
-                        .cornerRadius(8)
-                    Text("1.\n\n2.\n\n3.\n\n4.\n\n")
-                        .font(Font.custom("Livvic", size: 18))
-                        .foregroundColor(Color(red: 0.19, green: 0.24, blue: 0.25))
-                        .frame(width: 343, height: 491, alignment: .topLeading)
+                      .foregroundColor(.clear)
+                      .frame(width: 364, height: 416)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(8)
+                    
+                    
+                    
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.bottom, 272)
+                      .padding(.trailing, 146.0)
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.bottom, 272)
+                      .padding(.leading, 67.0)
+                    
+                    
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.bottom, 136)
+                      .padding(.trailing, 146.0)
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.bottom, 136)
+                      .padding(.leading, 67.0)
+                    
+                    
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.trailing, 146.0)
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.leading, 67.0)
+                   
+                   
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.top, 136)
+                      .padding(.trailing, 146.0)
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.top, 136)
+                      .padding(.leading, 67.0)
+                    
+                    
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.top, 272)
+                      .padding(.trailing, 146.0)
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 27, height: 46)
+                      .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                      .cornerRadius(15)
+                      .rotationEffect(Angle(degrees: -90))
+                      .padding(.top, 272)
+                      .padding(.leading, 67.0)
+                    
+                    
+                    VStack  {
+                        Spacer ()
+                        Text("Do you enjoy working with tools and machinery?\n                     Yes                    No\nDo you like outdoor activities and working with your hands?\n                     Yes                    No\nAre you interested in fixing or repairing things?\n                     Yes                    No\nDo you enjoy physical activities and sports?\n                     Yes                    No\nDo you prefer practical tasks over theoretical ones?\n                     Yes                    No")
+                            .font(Font.custom("Livvic", size: 18))
+                            .foregroundColor(Color(red: 0.19, green: 0.24, blue: 0.25))
+                            .frame(width: 343, height: 401, alignment: .topLeading)
+                    }
+                }
                     VStack {
-                        Spacer()
-                        Spacer()
-                        Spacer()
                        
                         ZStack {
+                            Rectangle()
+                              .foregroundColor(.clear)
+                              .frame(width: 37.67037, height: 64)
+                              .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
+                              .cornerRadius(15)
+                              .rotationEffect(Angle(degrees: -90))
+                              .padding(.leading, 140.0)
+                            
+                            Text("1/8")
+                              .font(Font.custom("Livvic", size: 18))
+                              .padding(.leading, 140.0)
+                              .foregroundColor(Color(red: 0.19, green: 0.24, blue: 0.25))
                             
                             Rectangle()
                                 .foregroundColor(.clear)
-                                .frame(width: 34, height: 64)
+                                .frame(width: 37.67037, height: 64)
                                 .background(Color(red: 0.32, green: 0.36, blue: 0.38).opacity(0.25))
                                 .cornerRadius(15)
                                 .rotationEffect(Angle(degrees: -90))
-                                .padding(.leading, 270.0)
+                                .padding(.leading, 300.0)
                             
                                  Image(systemName: "arrow.right")
                                      .foregroundColor(.init(red: 0.236, green: 0.266, blue: 0.286))
-                                     .padding(.leading, 266.754)
+                                     .padding(.leading, 300.0)
                                      .font(.system(size: 30))
                                      
-                        }
-      
+                        
                     }
                 }
             }
